201e58840fe9c3872f21650a88b049abe449ddee
Ben Smith <122588740+BenSmith1202@users.noreply.github.com>
2025-02-14 15:35:02 -0500

    Exponential Booms

--- src/fiveBigBooms.java
+++ src/fiveBigBooms.java
@@ -1,7 +1,11 @@
 public class fiveBigBooms {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("BOOM!");
+            System.out.print("BOO");
+            for (int j = 0; j < (Math.pow(3, i)); j++) {
+                System.out.print("OO");
+            }
+            System.out.println("M!");
         }
     }
 }
