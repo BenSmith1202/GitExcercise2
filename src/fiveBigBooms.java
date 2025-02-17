48034393152612ca1847a30bfa2d57dadb7f3651
Ben Smith <122588740+BenSmith1202@users.noreply.github.com>
2025-02-17 15:24:50 -0500

    Six big booms!?!??!?! Who Authorized this?????

--- src/fiveBigBooms.java
+++ src/fiveBigBooms.java
@@ -1,6 +1,6 @@
 public class fiveBigBooms {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             System.out.print("BO");
             for (int j = 0; j < (Math.pow(3, i)); j++) {
                 System.out.print("O");
