c9de6d435ebc2072b53478c4e43d45dc3d61921f
Ben Smith <122588740+BenSmith1202@users.noreply.github.com>
2025-02-17 15:15:14 -0500

    Removed extraneous 'o' from booms.

--- src/fiveBigBooms.java
+++ src/fiveBigBooms.java
@@ -1,9 +1,9 @@
 public class fiveBigBooms {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.print("BOO");
+            System.out.print("BO");
             for (int j = 0; j < (Math.pow(3, i)); j++) {
-                System.out.print("OO");
+                System.out.print("O");
             }
             System.out.println("M!");
         }
