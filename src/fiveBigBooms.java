4fa9c1d99990540a1504d5885a4c1b1b25b2f528
farleybm20 <farleybm20@gcc.edu>
2025-02-19 15:19:52 -0500

    made it even more enthusiastic

--- src/fiveBigBooms.java
+++ src/fiveBigBooms.java
@@ -1,6 +1,6 @@
 public class fiveBigBooms {
     public static void main(String[] args) {
-        System.out.println("YIPPPEEEE");
+        System.out.println("HUZZAH!!");
         for (int i = 0; i < 6; i++) {
             System.out.print("BO");
             for (int j = 0; j < (Math.pow(3, i)); j++) {
